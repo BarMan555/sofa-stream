e1d29966afa8e07cdc8bbc165d8676d2eb0d8b84
Boris Shesterikov <boris20033@gmail.com>
2026-04-23 15:50:50 +0400

    Add migrations
    Add migrations for Video entity.

---
+++ SofaStream.Infrastructure/Migrations/20260422201404_AddVideo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SofaStream.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVideo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "CurrentVideo_Duration",
+                table: "Rooms",
+                type: "interval",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentVideo_Title",
+                table: "Rooms",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentVideo_Url",
+                table: "Rooms",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentVideo_Duration",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentVideo_Title",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentVideo_Url",
+                table: "Rooms");
+        }
+    }
+}
