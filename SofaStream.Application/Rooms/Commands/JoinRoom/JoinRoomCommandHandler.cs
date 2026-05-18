c5dd5379b246a9a2d674c5d6ef571b56888b78a2
Boris Shesterikov <boris20033@gmail.com>
2026-05-18 23:05:15 +0400

    Fix logic for participant join in rooms

---
+++ SofaStream.Application/Rooms/Commands/JoinRoom/JoinRoomCommandHandler.cs
@@ -0,0 +1,21 @@
+using SofaStream.Application.Common.Interfaces;
+using SofaStream.Domain;
+using SofaStream.Domain.Common.Models;
+using SofaStream.Domain.Entities;
+
+namespace SofaStream.Application.Rooms.Commands.JoinRoom;
+
+public class JoinRoomCommandHandler(IRoomRepository roomRepository) : ICommandHandler<JoinRoomCommand, Result>
+{
+    public async Task<Result> HandleAsync(JoinRoomCommand command, CancellationToken cancellationToken = default)
+    {
+        var room = await roomRepository.GetByIdAsync(command.RoomId, cancellationToken);
+        if (room == null) return Result.Failure(DomainErrors.Room.NotFound);
+        
+        // Add spectator in room
+        room.AddParticipant(new RoomParticipant(command.UserId, isHost: false));
+        await roomRepository.UpdateAsync(room, cancellationToken);
+        
+        return Result.Success();
+    }
+}
