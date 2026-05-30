1e08f1bcb175a5a2c6cb2dabadb0fa9be48ebc9e
Boris Shesterikov <boris20033@gmail.com>
2026-05-31 00:02:07 +0400

    feat: add theme images and migrations

---
+++ SofaStream.Infrastructure/Migrations/20260529095351_AddRoomTheme.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SofaStream.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomTheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Theme",
+                table: "Rooms",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Dark");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Theme",
+                table: "Rooms");
+        }
+    }
+}
